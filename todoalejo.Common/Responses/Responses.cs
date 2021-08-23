b1d19eeaa905b81fc95a7f91ad5381ed2983c0f2
alejogo4 <alejo.go4@hotmail.com>
2021-08-22 20:44:55 -0500

    CRUD todo and init the schedule task

---
+++ todoalejo.Common/Responses/Responses.cs
@@ -0,0 +1,10 @@
+﻿namespace todoalejo.Common.Responses
+{
+    public class Responses
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+
+        public object Result { get; set; }
+    }
+}
